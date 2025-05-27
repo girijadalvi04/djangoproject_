5a9fd3a601f3e4c6b1a2b256c0dcd951bc5e1209
Girijadalvi0401 <girijadalvi1911@gmail.com>
2025-05-27 14:53:22 +0530

    Initial commit

---
+++ todoproject/todo/views.py
@@ -0,0 +1,26 @@
+
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'index.html', context)
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return redirect('/')
+
+    
